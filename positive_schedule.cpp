b1db8e3fb6ea5b516afb5d1d5f6e4a92d2097a02
Vardan Tuladhar <tuladharvr@vcu.edu>
2022-09-07 18:37:03 -0400

    Created the initial file for the schedule program

---
+++ positive_schedule.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main()
+{
+time_t t =  time(0);
+tm *now  =  localtime(&t);
+string todays_year = to_string(now->tm_year + 1900);
+string todays_month = to_string(now -> tm_mon +1);
+string todays_day = to_string( now-> tm_mday);
+string todays_date;
+todays_date.append(todays_month);
+todays_date.append("-");
+todays_date.append(todays_day);
+todays_date.append("-");
+todays_date.append(todays_year);
+todays_date.append(".txt");
+ofstream schedule(todays_date);
+
+schedule.close();
+
+
+
+}
